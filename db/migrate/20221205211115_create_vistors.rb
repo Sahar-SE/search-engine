9711f1d45d5ee226b00706b0252004d4e588cdf1
rich <adukyerer@gmail.com>
2022-12-05 21:16:34 +0000

    Add Migrations schemas

--- db/migrate/20221205211115_create_vistors.rb
+++ db/migrate/20221205211115_create_vistors.rb
@@ -1,6 +1,7 @@
 class CreateVistors < ActiveRecord::Migration[7.0]
   def change
     create_table :vistors do |t|
+      t.string :user_agent
 
       t.timestamps
     end
--- db/migrate/20221205211143_create_searches.rb
+++ db/migrate/20221205211143_create_searches.rb
@@ -1,6 +1,9 @@
 class CreateSearches < ActiveRecord::Migration[7.0]
   def change
     create_table :searches do |t|
+      t.string :method, null: false
+      t.string :params
+      t.references :visitor, null: false, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
