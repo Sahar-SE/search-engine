bda36c424d376a150cbf312c7c66d9526622579e
rich <adukyerer@gmail.com>
2022-12-23 10:03:24 +0000

    Create New song model

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_233753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "searches", force: :cascade do |t|
     t.string "params"
     t.bigint "visitor_id", null: false
@@ -28,10 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_233753) do
     t.index ["visitor_id"], name: "index_searches_on_visitor_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
+  create_table "songs", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "visitors", force: :cascade do |t|
