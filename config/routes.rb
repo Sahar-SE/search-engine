97161fabb1ba770428dff98efa54ba7a93de3c87
rich <adukyerer@gmail.com>
2022-12-28 20:44:42 +0000

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "pages#search"
+  get "movies/:id", to: "pages#show", as: :movie
+  resources :movies, only: [:show]
   get "search", to: "pages#search", as: :search
   post "enable_analytics", to: "analytics#enable", as: :enable_analytics
   get  "analytics", to: "analytics#index"
   get  "history", to: "pages#history"
+  post  "sign_in", to: "users#ssign_in"
+  post  "sign_up", to: "users#sign_up"
+  get   "sign_up", to: "pages#sign_up"
+  get   "sign_in", to: "pages#sign_in"
 end
