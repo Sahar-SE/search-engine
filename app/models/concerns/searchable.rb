64fac54f54b486edb87e5423a5623886868bd57a
rich <adukyerer@gmail.com>
2022-12-28 20:47:07 +0000

    Add elastic search config

---
+++ app/models/concerns/searchable.rb
@@ -0,0 +1,31 @@
+module Searchable
+    extend ActiveSupport::Concern
+  
+    included do
+      include Elasticsearch::Model
+      include Elasticsearch::Model::Callbacks
+
+      mapping do
+        indexes :actors, type: :text
+        indexes :title, type: :text
+        indexes :description, type: :text
+        indexes :genre, type: :keyword
+        indexes :country, type: :keyword
+      end
+  
+      def self.search(query)
+        params = {
+      query: {
+        bool: {
+          should: [
+            { match: { title: { query: query, fuzziness: "AUTO"  }}},
+            { match: { description: { query: query} }},
+            { match: {country: query }},
+          ],
+        }
+      },
+    }
+        self.__elasticsearch__.search(params)
+      end
+    end
+  end
