dad10e0f7f40f293da45aa4ba8b89b22359e580b
rich <adukyerer@gmail.com>
2022-12-28 20:42:07 +0000

    Replace with elastic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,22 @@ class PagesController < ApplicationController
   include TrackSearch
   include SetCurrentVisitor
   before_action :track_search, only: [:search]
+  before_action :update_actual_search, only: [:show]
 
   def search
     @most_searched = Search.searches_ordered_limited
-    @articles = Article.where("name LIKE ?", "%#{params[:search]}%")
+    query = params[:search] || ""
+    @results = Movie.search(query).results
+  end
+  
+  def show
+    @movie = Movie.find(params[:id])
   end
 
   def history
-    @visitor_recent_search = Visitor.find(session[:visitor_id]).searches.last(4)
+    @visitor_recent_search = Current.visitor.searches.includes(:movie).order(created_at: :desc).limit(10)
+  end
+
+  def sign_in
   end
 end
