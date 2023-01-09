a82f7f607bdac02b7202af530981b16098cd4941
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-01-09 19:11:02 +0430

    Creat User migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_221026) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_142328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,12 +34,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_221026) do
     t.index ["visitor_id"], name: "index_searches_on_visitor_id"
   end
 
-  create_table "visitors", force: :cascade do |t|
-    t.string "user_agent"
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "visitors", force: :cascade do |t|
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_visitors_on_user_id"
+  end
+
   add_foreign_key "searches", "movies", on_delete: :cascade
   add_foreign_key "searches", "visitors", on_delete: :cascade
+  add_foreign_key "visitors", "users", on_delete: :cascade
 end
