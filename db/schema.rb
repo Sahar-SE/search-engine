caa2f0db71154b2d3899085e0f4087b36f7c5f9a
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-01-09 18:43:26 +0430

    Add visitor foregeign key to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_064627) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_221026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,32 +34,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_064627) do
     t.index ["visitor_id"], name: "index_searches_on_visitor_id"
   end
 
-  create_table "songs", force: :cascade do |t|
-    t.string "title"
-    t.string "artist"
-    t.string "genre"
-    t.text "lyrics"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
   create_table "visitors", force: :cascade do |t|
     t.string "user_agent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_visitors_on_user_id"
   end
 
   add_foreign_key "searches", "movies", on_delete: :cascade
   add_foreign_key "searches", "visitors", on_delete: :cascade
-  add_foreign_key "visitors", "users", on_delete: :cascade
 end
