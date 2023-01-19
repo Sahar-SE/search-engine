b888d2d08830efb3ae53dc3225137d4da2be4571
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-01-19 21:59:22 +0430

    Add targets with javascript

--- app/javascript/controllers/showhide_controller.js
+++ app/javascript/controllers/showhide_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="showhide"
 export default class extends Controller {
+  static targets = ["input", "output"]
   connect() {
   }
 }
